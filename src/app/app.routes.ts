3f8fff7f904b43d530b8cfe4b554cd85e10f6dab
Karen Gomes <karenecris12015@gmail.com>
2025-01-08 21:28:32 -0300

    ajustes em login, implementação de formulário em login. Criação de pastas para guardar os services e models/interfaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Definindo tela inicial da app (login)
+  { path: 'home', component: HomeComponent } // Definindo rota para a tela home
 ];
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Router } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatSelectModule, MatIconModule, MatDividerModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  form!: FormGroup;
+
+  constructor(private route: Router, private formBuilder: FormBuilder){
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: [null, [
+              Validators.required,
+              Validators.email
+            ]],
+      senha: [null, [
+              Validators.required,
+              // Validators.minLength(6)
+            ]]
+    });
+  }
+
+  submitForm() {
+    if(this.form.valid){
+      console.log(this.form.value)
+      this.route.navigate(["/home"]);
+    }else {
+      console.log(`formulário inválido`)
+    }
+  }
 }
